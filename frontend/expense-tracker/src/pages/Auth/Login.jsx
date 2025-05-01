4af5701558e35e9fbe6d01a8d07e6b49f668dbdb
Ritika Sah <ritikasah731@gmail.com>
2025-05-01 22:53:06 +0530

    CHANGES

--- frontend/expense-tracker/src/pages/Auth/Login.jsx
+++ frontend/expense-tracker/src/pages/Auth/Login.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import AuthLayout from '../../components/layouts/AuthLayout'
+import {useNavigate} from 'react-router-dom'
+import Input from '../../components/Inputs/Input.jsx'
 
 const Login = () => {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [error, setError] = useState(null)
+
+  const navigate = useNavigate();
+
+  // Handle login Form Submit 
+  const handleLogin = async(e)=>{
+
+  }
+
   return (
     <AuthLayout>
       <div className="lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center">
@@ -9,6 +22,26 @@ const Login = () => {
       <p className="text-xs text-slate-700 mt-[5px] mb-6">
         Please enter your details to log in
       </p>
+        
+
+      <form onSubmit={handleLogin}>
+        <Input
+        value={email}
+        onChange={({target})=> setEmail(target.value)}
+        label="Email Address"
+        placeholder="emily@example.com"
+        type="text"
+        />
+        <Input
+        value={password}
+        onChange={({target})=> setEmail(target.value)}
+        label="Password"
+        placeholder="Min 8 Charcaters"
+        type="password"
+        />
+      </form>
+
+
       </div>
     </AuthLayout>
   )
