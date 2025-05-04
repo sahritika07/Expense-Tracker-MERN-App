ff3f2697e2686c3e096dea9e2a21d36ad4cb029c
Ritika Sah <ritikasah731@gmail.com>
2025-05-04 23:36:45 +0530

    register function

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -36,6 +36,38 @@ exports.registerUser = async (req, res) => {
     }
 };
 
-exports.loginUser = async(req,res)=>{};
+exports.loginUser = async(req,res)=>{
+    const {email,password} = req.body;
+    if(!email || !password){
+        return res.status(400).json({message: "All fields are required"});
+    }
 
-exports.getUserInfo = async(req,res)=>{};
+    try {
+        const user = await User.findOne({email});
+        if(!user || !(await user.comparePassword(password))){
+            return res.status(400).json({message: "Invalid Credentials"});
+        } 
+
+        res.status(200).json({
+            id: user._id,
+            user,
+            token: generateToken(user._id),
+        })
+
+    } catch (error) {
+        res.status(500).json({message: "Error while registering user", error:error.message})
+    }
+
+};
+
+exports.getUserInfo = async(req,res)=>{
+    try {
+        const user = await User.findById(req.user.id).select("-password")
+        if(!user){
+            return res.status(404).json({message: "User not found"});
+        }
+        return res.status(200).json(user);
+    } catch (error) {
+        res.status(500).json({message: "Error while registering user", error:error.message})
+    }
+};
