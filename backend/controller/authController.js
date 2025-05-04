a3052c47ac44750aefb036b9c005e319b643c68b
Ritika Sah <ritikasah731@gmail.com>
2025-05-04 21:32:17 +0530

    register function

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -5,7 +5,36 @@ const generateToken = (id) =>{
     return jwt.sign({id}, process.env.JWT_SECRET, {expiresIn: "1h"})
 };
 
-exports.registerUser = async (req, res) => {};
+exports.registerUser = async (req, res) => {
+    const {fullName, email, password, profileImageUrl} = req.body;
+
+    if(!fullName || !email || !password){
+      return res.status(400).json({message: "All fields are required"});
+    }
+
+    try {
+        const existingUser = await User.findOne({email});
+        if(existingUser){
+            return res.status(400).json({message: "Email already in use"});
+        }
+      
+        const user = await User.create({
+            fullName,
+            email,
+            password,
+            profileImageUrl
+        })
+
+        res.status(201).json({
+            id: user._id,
+            user,
+            token: generateToken(user._id),
+        })
+
+    } catch (error) {
+        res.status(500).json({message: "Error while registering user", error:error.message})
+    }
+};
 
 exports.loginUser = async(req,res)=>{};
 
