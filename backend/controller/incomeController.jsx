7106765029d1e835e3ff17f4b8edc62ffee3d076
Ritika Sah <ritikasah731@gmail.com>
2025-05-07 10:35:14 +0530

    Creating income schema

---
+++ backend/controller/incomeController.jsx
@@ -0,0 +1,10 @@
+const User = require("../models/User");
+const Income = require("../models/Income")
+
+exports.addIncome = async(req,res)=> {}
+
+exports.getAllIncome = async(req,res)=> {}
+
+exports.deleteIncome = async(req,res)=> {}
+
+exports.downloadIncomeExcel = async(req,res)=> {}
---
+++ backend/routes/incomeRoutes.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const {addIncome, getAllIncome, deleteIncome, downloadIncomeExcel} = require("../controller/incomeControllere");
+const {protect} = require("../middleware/authMiddleware")
+
+const router = express.Router();
+
+router.post("/add", protect, addIncome);
+router.get("/get", protect, getAllIncome);
+router.get("/downloadexcel", protect, downloadIncomeExcel);
+router.delete("/:id", protect, deleteIncome);
+
+module.exports = router;
