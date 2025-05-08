a600208bde3696a21763bd76d9af36c135512200
Ritika Sah <ritikasah731@gmail.com>
2025-05-08 22:33:01 +0530

    creating dashboard controller and dashboard routes

--- backend/server.js
+++ backend/server.js
@@ -27,6 +27,7 @@ app.use("/api/v1/income", incomeRoutes);
 app.use("/api/v1/expense", expenseRoutes);
 app.use("/api/v1/dashboard", dashboardRoutes);
 
+
 //Server uploads folder
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
