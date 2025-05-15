b8654774c7d4688cfa53846d558bcac9b4708486
Ritika Sah <ritikasah731@gmail.com>
2025-05-15 22:10:51 +0530

    Creating piecharts

---
+++ frontend/expense-tracker/src/components/Charts/CustomPieChart.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import {
+    PieChart,
+    Pie,
+    Cell,
+    Tooltip,
+    ResponsiveContainer,
+    Legend,
+} from "recharts";
+
+const CustomPieChart = ({data,label,totalAmount,colors,showTextAnchor}) => {
+  return (
+    <ResponsiveContainer/>
+  )
+}
+
+export default CustomPieChart
