91832e45841f38f604757c755c99a572bae00310
Ritika Sah <ritikasah731@gmail.com>
2025-07-09 22:28:34 +0530

    Changes

--- backend/server.js
+++ backend/server.js
@@ -32,5 +32,6 @@ app.use("/api/v1/dashboard", dashboardRoutes);
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
+
 const PORT = process.env.PORT || 5000
 app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`))
